1771de73f341bbe9572d4b6955c424ad60782d12
asunny9333 <asunny9333@conestogac.on.ca>
2024-07-07 22:30:52 -0400

    added route for user

---
+++ routes/userRoutes.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+// Get all users
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Create a new user
+router.post('/', async (req, res) => {
+    const user = new User(req.body);
+    try {
+        const newUser = await user.save();
+        res.status(201).json(newUser);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Get a single user
+router.get('/:id', getUser, (req, res) => {
+    res.json(res.user);
+});
+
+// Update a user
+router.put('/:id', getUser, async (req, res) => {
+    if (req.body.email != null) {
+        res.user.email = req.body.email;
+    }
+    if (req.body.password != null) {
+        res.user.password = req.body.password;
+    }
+    if (req.body.username != null) {
+        res.user.username = req.body.username;
+    }
+    if (req.body.purchaseHistory != null) {
+        res.user.purchaseHistory = req.body.purchaseHistory;
+    }
+    if (req.body.shippingAddress != null) {
+        res.user.shippingAddress = req.body.shippingAddress;
+    }
+    try {
+        const updatedUser = await res.user.save();
+        res.json(updatedUser);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Delete a user
+router.delete('/:id', getUser, async (req, res) => {
+    try {
+        await res.user.remove();
+        res.json({ message: 'User deleted' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+async function getUser(req, res, next) {
+    let user;
+    try {
+        user = await User.findById(req.params.id);
+        if (user == null) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+    res.user = user;
+    next();
+}
+
+module.exports = router;
